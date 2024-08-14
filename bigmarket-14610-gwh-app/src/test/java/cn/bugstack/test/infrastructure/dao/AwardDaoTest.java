15512c982e74eee24eef76acb1f67ae74762afe5
weixin_52875810 <842123094@qq.com>
2024-08-14 13:40:30 +0800

    orm框架测试开发

---
+++ bigmarket-14610-gwh-app/src/test/java/cn/bugstack/test/infrastructure/dao/AwardDaoTest.java
@@ -0,0 +1,9 @@
+package cn.bugstack.test.infrastructure.dao;
+/**
+ * @author fuzhouling
+ * @date 2024/08/14
+ * @program bigmarket-14610-gwh
+ * @description todo desc...
+ **/
+public class AwardDaoTest {
+}
